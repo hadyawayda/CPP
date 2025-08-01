ee06e464f36c2733f01165ede0fd6410fee015be
hadyawayda <hady_awayda@hotmail.com>
2025-08-01 23:09:42 +0300

    Updated ex02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -3,15 +3,15 @@
 
 int main() {
     std::string brain = "HI THIS IS BRAIN";
-    std::string *ptr = &brain;
-    std::string &ref = brain;
+    std::string *stringPTR = &brain;
+    std::string &stringREF = brain;
 
     std::cout << "Address of brain: " << &brain << std::endl;
-    std::cout << "Address held by ptr: " << ptr << std::endl;
-    std::cout << "Address held by ref: " << &ref << std::endl;
+    std::cout << "Address held by ptr: " << stringPTR << std::endl;
+    std::cout << "Address held by ref: " << &stringREF << std::endl;
 
     std::cout << "Value of brain: " << brain << std::endl;
-    std::cout << "Value pointed by ptr: " << *ptr << std::endl;
-    std::cout << "Value pointed by ref: " << ref << std::endl;
+    std::cout << "Value pointed by ptr: " << *stringPTR << std::endl;
+    std::cout << "Value pointed by ref: " << stringREF << std::endl;
     return 0;
 }
